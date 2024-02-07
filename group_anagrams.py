c4ac5ff9b1c3a6d4cfaaf03323ed2e93a5729d3e
seharkansal <seharkansal1@gmail.com>
2024-02-06 19:00:51 -0500

    6th feb

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,3 +1,17 @@
+'''
+Objective:
+The groupAnagrams method is designed to group a list of words into anagrams.
+Data Structure:
+It utilizes a dictionary (d) to store anagram groups. The key for each group is the sorted version of the words.
+Processing:
+1. It iterates through each word in the input list (words).
+2. For each word, it creates a key (s) by sorting its characters using "".join(sorted(i)).
+3. Checks if the key is already present in the dictionary (d).
+4. If not present, it adds a new entry with the key and a list containing the word.
+5. If already present, it appends the word to the existing list associated with that key.
+Return Value:
+The method returns the values (anagram groups) of the dictionary (d.values()).
+'''
 words = ["eat","tea","tan","ate","nat","bat"]
 # Dictionary to store anagram groups
 anagram_groups = {}
